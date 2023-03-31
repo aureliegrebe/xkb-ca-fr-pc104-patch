255974c2c67a883ff64f9a20f38cf6d7b856ef4f
Aurélie Grebe <aureliegrebe@gmail.com>
2023-03-31 13:18:06 -0400

    Add degree and guillemet patch
    Added install script

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # xkb-ca-fr-pc104-patch
+
+Patches to add guillemet and degree symbols to the Canadian French keyboard
+layout that are accessible on a pc104 layout. 
+
+## Requirements
+
+These patches are for X11 keyboard files. I have only tested this script on
+an Arch installation running X11, so some modification may be necessary if
+the layouts are stored in different places on your system.
+
+## installation
+
+1. Run `install.sh` as superuser.
+2. Reload keyboard layout: `setxkbmap -layout ca -variant fr`
+
+
+## Symbol mapping
+| Symbol | Keystroke |
+|--------|-----------|
+| '«'    | AltGr + Z |
+| '»'    | AltGr + X |
+| '°'    | AltGr + D |
---
+++ install.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+patches=( guillemet.patch degree.patch )
+
+usage="$(basename "$0") [-h] [-f LAYOUT_FILE] [-b BACKUP_FILE]
+Installs patches to X11 keyboard layout files
+
+where:
+    -h  show this help text
+    -f  specify layout_file
+	-b  specify bakup file location"
+
+key_file=/usr/share/X11/xkb/symbols/ca
+bak_file=$key_file.BAK
+
+while getopts ':hfb:' option; do
+  case "$option" in
+    h|--help) echo "$usage"
+       exit
+       ;;
+    f) key_file=$OPTARG
+       ;;
+    :) printf "missing argument for -%s\n" "$OPTARG" >&2
+       echo "$usage" >&2
+       exit 1
+       ;;
+    b) bak_file=$OPTARG
+       ;;
+    :) printf "missing argument for -%s\n" "$OPTARG" >&2
+       echo "$usage" >&2
+       exit 1
+       ;;
+   \?) printf "illegal option: -%s\n" "$OPTARG" >&2
+       echo "$usage" >&2
+       exit 1
+       ;;
+  esac
+done
+shift $((OPTIND - 1))
+
+# Backup file
+cp -i $key_file $bak_file
+
+# Apply patches
+for PATCH in "${patches[@]}"
+do
+	patch $key_file $PATCH
+done
+
+
